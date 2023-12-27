fc7e883f3445a8184317f5d937e9c746c8c0ff3a
ComNewbie <iforking3@naver.com>
2023-12-28 00:39:29 +0900

    #feat Intake서비스 초기설정

---
+++ sosik-intake-service/src/test/java/com/example/sosikintakeservice/SosikIntakeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sosikintakeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosikIntakeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
