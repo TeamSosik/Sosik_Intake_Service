b36da8b9137d034f02d0bcda51a575bc183d067b
dahye <oda0103@naver.com>
2024-01-08 15:55:24 +0900

    Feat : 일일 목표 칼로리 조회 기능 구현
    일일 목표 칼로리 조회 기능을 구현하였습니다.

--- sosik-intake-service/src/main/java/com/example/sosikintakeservice/repository/TargetCalorieRepository.java
+++ sosik-intake-service/src/main/java/com/example/sosikintakeservice/repository/TargetCalorieRepository.java
@@ -2,8 +2,13 @@ package com.example.sosikintakeservice.repository;
 
 import com.example.sosikintakeservice.model.entity.DayTargetCalorieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface TargetCalorieRepository extends JpaRepository<DayTargetCalorieEntity, Long> {
-    Optional<DayTargetCalorieEntity>findTopByOrderByLastCreatedDateDesc();
+    Optional<DayTargetCalorieEntity> findTopByOrderByLastCreatedDateDesc();
+
+    List<DayTargetCalorieEntity> findByMemberIdAndCreateAt(Long memberId, LocalDateTime localDateTime);
 }
--- sosik-intake-service/src/test/java/com/example/sosikintakeservice/service/DayTargetCalorieServiceImplTest.java
+++ sosik-intake-service/src/test/java/com/example/sosikintakeservice/service/DayTargetCalorieServiceImplTest.java
@@ -2,6 +2,7 @@ package com.example.sosikintakeservice.service;
 
 import com.example.sosikintakeservice.dto.request.RequestTargetCalorie;
 import com.example.sosikintakeservice.dto.request.UpdateTargetCalorie;
+import com.example.sosikintakeservice.dto.response.ResponseGetDayTargetCalorie;
 import com.example.sosikintakeservice.exception.ApplicationException;
 import com.example.sosikintakeservice.model.entity.DayTargetCalorieEntity;
 import com.example.sosikintakeservice.repository.TargetCalorieRepository;
@@ -14,6 +15,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,17 +35,17 @@ class DayTargetCalorieServiceImplTest {
     private TargetCalorieRepository targetCalorieRepository;
     @Mock
     private Optional<DayTargetCalorieEntity> targetCalorieEntity = Optional.of(testTargetCalorie());
+
+    private ResponseGetDayTargetCalorie expectedResponse = testGetDayTargetCalorieDTO();
     @Mock
     private LocalDateTime currentTime = LocalDateTime.now();
     @Mock
     private LocalDateTime lastCreatedDate;
 
 
-
-
     @DisplayName("일일목표칼로리 기입에 성공한다.")
     @Test
-    void givenTestTargetCalorieWhenCreateTargetCalorieThenSuccess(){
+    void givenTestTargetCalorieWhenCreateTargetCalorieThenSuccess() {
 
         RequestTargetCalorie testTargetCalorieDto = testTargetCalorieDto();
 
@@ -51,70 +54,89 @@ class DayTargetCalorieServiceImplTest {
         assertThat(dayTargetCalorieService.createTargetCalorie(testTargetCalorieDto)).isEqualTo(testTargetCalorieDto);
 
     }
+
     @DisplayName("일일목표칼로리 기입에 실패한다. - 오늘 등록했음")
     @Test
-    void givenTestTargetCalorieWhenCreateTargetCalorieThrowEXISTENCE_TARGETCALORIE_ERROR(){
+    void givenTestTargetCalorieWhenCreateTargetCalorieThrowEXISTENCE_TARGETCALORIE_ERROR() {
 
         RequestTargetCalorie testTargetCalorieDto = testTargetCalorieErrorDto();
         given(targetCalorieRepository.findTopByOrderByLastCreatedDateDesc()).willReturn(targetCalorieEntity);
         when(targetCalorieEntity.get().getCreatedAt()).thenReturn(currentTime);
         given(lastCreatedDate).willReturn(LocalDateTime.now());
-        assertThatThrownBy(()-> dayTargetCalorieService.createTargetCalorie(testTargetCalorieDto))
+        assertThatThrownBy(() -> dayTargetCalorieService.createTargetCalorie(testTargetCalorieDto))
                 .isInstanceOf(ApplicationException.class);
     }
 
     @DisplayName("일일목표칼로리 수정에 성공한다.")
     @Test
-    void givenTestTargetCalorieWhenUpdateTargetCalorieThenSuccess(){
+    void givenTestTargetCalorieWhenUpdateTargetCalorieThenSuccess() {
         UpdateTargetCalorie updateTargetCalorie = testUpdateTargetCalorieDto();
         given(targetCalorieRepository.findById(updateTargetCalorie.Id())).willReturn(Optional.ofNullable(dayTargetCalorieEntity));
         dayTargetCalorieEntity.updateTargetCalorie(updateTargetCalorie);
         assertThat(dayTargetCalorieService.updateDayTargetCalorie(updateTargetCalorie)).isEqualTo("ok");
 
     }
+
     @DisplayName("일일목표칼로리 수정에 실패한다. - updateDTO id로 찾는 entity가 없을 때")
     @Test
-    void givenTestTargetCalorieWhenUpdateTargetCalorieThrowTARGETCALORIE_NOT_FOUND(){
+    void givenTestTargetCalorieWhenUpdateTargetCalorieThrowTARGETCALORIE_NOT_FOUND() {
         UpdateTargetCalorie updateTargetCalorie = testUpdateTargetCalorieErrorDto();
         given(targetCalorieRepository.findById(updateTargetCalorie.Id())).willReturn(Optional.empty());
-        assertThatThrownBy(()-> dayTargetCalorieService.updateDayTargetCalorie(updateTargetCalorie))
+        assertThatThrownBy(() -> dayTargetCalorieService.updateDayTargetCalorie(updateTargetCalorie))
                 .isInstanceOf(ApplicationException.class);
 
     }
 
-    private static RequestTargetCalorie testTargetCalorieDto(){
+    private static RequestTargetCalorie testTargetCalorieDto() {
         return RequestTargetCalorie.builder()
                 .memberId(1L)
                 .dayTargetKcal(2000)
                 .dailyIntakePurpose(1)
                 .build();
     }
-    private static RequestTargetCalorie testTargetCalorieErrorDto(){
+
+    private static RequestTargetCalorie testTargetCalorieErrorDto() {
         return RequestTargetCalorie.builder()
                 .memberId(1L)
                 .dailyIntakePurpose(1)
                 .build();
     }
 
-    private static UpdateTargetCalorie testUpdateTargetCalorieDto(){
+    private static UpdateTargetCalorie testUpdateTargetCalorieDto() {
         return UpdateTargetCalorie.builder()
                 .Id(1L)
                 .dayTargetKcal(5000)
                 .dailyIntakePurpose(1)
                 .build();
     }
-    private static UpdateTargetCalorie testUpdateTargetCalorieErrorDto(){
+
+    private static UpdateTargetCalorie testUpdateTargetCalorieErrorDto() {
         return UpdateTargetCalorie.builder()
                 .Id(1L)
                 .dailyIntakePurpose(1)
                 .build();
     }
 
-    private static DayTargetCalorieEntity testTargetCalorie(){
+    private static DayTargetCalorieEntity testTargetCalorie() {
         return DayTargetCalorieEntity.builder()
                 .memberId(1L)
                 .dayTargetKcal(2000)
                 .dailyIntakePurpose(1)
+                .build();
+    }
+
+    @DisplayName("일일 목표 칼로리 조회에 성공하였습니다.")
+    @Test
+    void givenTestDayTargetCalorieWhenGetDayTargetCalorieThenSuccess() {
+        ResponseGetDayTargetCalorie getDayTargetCalorie = testGetDayTargetCalorieDTO();
+        assertThat(getDayTargetCalorie).isEqualTo(expectedResponse);
+    }
+
+    private static ResponseGetDayTargetCalorie testGetDayTargetCalorieDTO() {
+        return ResponseGetDayTargetCalorie.builder()
+                .memberId(1L)
+                .dayTargetKcal(1000)
+                .dailyIntakePurpose(2000)
                 .build();
     }
 }
