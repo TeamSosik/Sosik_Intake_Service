38b93c2906573428409c371513b2019b58896015
이호진 <gommind@naver.com>
2024-01-15 13:06:27 +0900

    feat 생성날짜를 Between으로 조회하는 기능 생성

--- sosik-intake-service/src/main/java/com/example/sosikintakeservice/repository/IntakeRepository.java
+++ sosik-intake-service/src/main/java/com/example/sosikintakeservice/repository/IntakeRepository.java
@@ -4,10 +4,12 @@ import com.example.sosikintakeservice.dto.response.ResponseGetIntake;
 import com.example.sosikintakeservice.model.entity.IntakeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IntakeRepository extends JpaRepository<IntakeEntity, Long> {
-    List<IntakeEntity> findByMemberIdAndCreatedAt(Long memberId,LocalDateTime localDateTime);
+    List<IntakeEntity> findByMemberIdAndCreatedAt(Long memberId, LocalDate localDateTime);
 
+    List<IntakeEntity> findByMemberIdAndCreatedAtBetween(Long memberId, LocalDate start2, LocalDate end);
 }
---
+++ sosik-intake-service/src/test/java/com/example/sosikintakeservice/repository/IntakeRepositoryTest.java
@@ -0,0 +1,109 @@
+package com.example.sosikintakeservice.repository;
+
+import com.example.sosikintakeservice.model.entity.Category;
+import com.example.sosikintakeservice.model.entity.IntakeEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class IntakeRepositoryTest {
+
+    @Autowired
+    IntakeRepository intakeRepository;
+
+    @DisplayName("respository 잘 띄워지는지 테스트")
+    @Test
+    void givenWhenThenSuccessRespotioryPressent() {
+
+
+        assertThat(intakeRepository).isNotNull();
+    }
+
+    @DisplayName("intake조회 사이즈가0")
+    @Test
+    void givenWhenFindIntakeByMemberIdAndCreatedAtBetweenThenSizeIs0() {
+
+        // given
+
+        // when
+        Long memberId = 1L;
+        LocalDate start = LocalDate.now();
+        LocalDate end = LocalDate.now();
+
+        System.out.println("start : " + start);
+        System.out.println("end : " + end);
+
+        List<IntakeEntity> intakeList = intakeRepository.findByMemberIdAndCreatedAtBetween(memberId, start, end);
+
+        // then
+        assertThat(intakeList.size()).isEqualTo(0);
+    }
+
+    @DisplayName("intake조회 사이즈가2")
+    @Test
+    void givenWhenFindIntakeByMemberIdAndCreatedAtBetweenThenSizeIs2() {
+
+        // given
+        IntakeEntity intake1 = IntakeEntity.builder()
+                .memberId(1L)
+                .dayTargetCalorieId(1L)
+                .foodId(1L)
+                .category(Category.BREAKFAST)
+                .build();
+        IntakeEntity intake2 = IntakeEntity.builder()
+                .memberId(1L)
+                .dayTargetCalorieId(1L)
+                .foodId(1L)
+                .category(Category.BREAKFAST)
+                .build();
+        IntakeEntity intake3 = IntakeEntity.builder()
+                .memberId(1L)
+                .foodId(1L)
+                .dayTargetCalorieId(1L)
+                .category(Category.BREAKFAST)
+                .build();
+
+        intakeRepository.save(intake1);
+        intakeRepository.save(intake2);
+        intakeRepository.save(intake3);
+
+        // update intake3 createdAt
+        intake3.setCreatedAt(LocalDate.now().minusDays(1));
+
+        // when
+        Long memberId = 1L;
+        LocalDate start = LocalDate.now();
+        LocalDate end = LocalDate.now();
+
+        System.out.println("start : " + start);
+        System.out.println("end : " + end);
+
+        List<IntakeEntity> intakeList = intakeRepository.findByMemberIdAndCreatedAtBetween(memberId, start, end);
+
+        // then
+        assertThat(intakeList.size()).isEqualTo(2);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
