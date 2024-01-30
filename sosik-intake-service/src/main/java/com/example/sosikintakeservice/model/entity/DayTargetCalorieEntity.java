178a4e5354f29c0931051611c832aa70bf017a4f
이호진 <gommind@naver.com>
2024-01-30 17:48:21 +0900

    style import 정리

--- sosik-intake-service/src/main/java/com/example/sosikintakeservice/model/entity/DayTargetCalorieEntity.java
+++ sosik-intake-service/src/main/java/com/example/sosikintakeservice/model/entity/DayTargetCalorieEntity.java
@@ -1,7 +1,12 @@
 package com.example.sosikintakeservice.model.entity;
 
 import com.example.sosikintakeservice.dto.request.RequestUpdateTargetCalorie;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
