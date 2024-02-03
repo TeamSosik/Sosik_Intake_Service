3c2a33f00c1f8ad14e9b7af4a090c505c28fca1b
ComNewbie <iforking3@naver.com>
2024-02-04 04:11:33 +0900

    refactor: 서비스로직에서 팩토리메서드 패턴으로 변경
    ## 🏷 작업 분류
    
    - [ ]  기능 추가
    - [x]  리팩토링
    - [ ]  버그 수정
    - [ ]  환경 수정 및 기타
    
    ## 💡 작업 개요
    목표칼로리 서비스 로직부분에서 빌더패턴으로 작성되던것을 팩토리 매서드 패턴으로 변경하였습니다!
    ## ***📜** 리뷰 요청사항*

--- sosik-intake-service/src/main/java/com/example/sosikintakeservice/dto/response/ResponseGetDayTargetCalorie.java
+++ sosik-intake-service/src/main/java/com/example/sosikintakeservice/dto/response/ResponseGetDayTargetCalorie.java
@@ -1,7 +1,13 @@
 package com.example.sosikintakeservice.dto.response;
 
+import com.example.sosikintakeservice.model.entity.DayTargetCalorieEntity;
 import lombok.Builder;
 
 @Builder
 public record ResponseGetDayTargetCalorie(Integer dayTargetKcal) {
+    public static ResponseGetDayTargetCalorie buildResponseGetDayTargetCalorie(DayTargetCalorieEntity dayTargetCalorieEntity){
+        return ResponseGetDayTargetCalorie.builder()
+                .dayTargetKcal(dayTargetCalorieEntity.getDayTargetKcal())
+                .build();
+    }
 }
