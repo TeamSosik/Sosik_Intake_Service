e185dbb37f2949f9678e25d94c67fd4216bcae12
minutaurus <made_power@naver.com>
2024-02-12 13:56:05 +0900

    aws 배포 셋팅

--- sosik-intake-service/src/main/java/com/example/sosikintakeservice/SosikIntakeServiceApplication.java
+++ sosik-intake-service/src/main/java/com/example/sosikintakeservice/SosikIntakeServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
 public class SosikIntakeServiceApplication {
-
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(SosikIntakeServiceApplication.class, args);
 	}
 
